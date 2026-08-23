6aabed81399a66fece2a7570cbc84b0ed3af99be
yazenthesmart <yzaensalih@gmail.com>
2026-08-23 21:16:15 +0300

    Update uv.sw.js

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,11 +1,11 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.bundle.js');
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.config.js');
-importScripts(__uv$config.sw);
-
-const uv = new UVServiceWorker();
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        uv.fetch(event)
-    );
-});
+self.__uv$config = {
+    prefix: '/uv/service/',
+    bare: 'https://bare.benrogo.net/', // Active public bare server
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
